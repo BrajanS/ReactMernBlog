c8edc81c298ed5dec9dc0ff4e0cc0f8deeb5a57f
BrajanS <szczapbrajan33@gmail.com>
2024-10-01 16:08:19 +0200

    1.01

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"], // J'ai ajouter "jsx", supprime si ça casse quelque chose
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
