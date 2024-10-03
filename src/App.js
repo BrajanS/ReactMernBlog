1cca241de76a94c41b8c56359e273d5ea6538739
BrajanS <szczapbrajan33@gmail.com>
2024-10-03 09:33:39 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import Home from './pages/Home.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 export default function App() {
   return (
     <div className="App">
-      <Home/>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}>Acceuil</Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
